18bba0254ff20f54bbd4aa7fb40f9fb3ec780df5
oranelbk <98135480+oranelbk@users.noreply.github.com>
2024-03-17 18:07:36 +0100

    Add files via upload

---
+++ trello/script.js
@@ -0,0 +1,29 @@
+function openModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  var draggedElement = document.getElementById(data);
+  var dropZone = event.target;
+
+  // Check if the drop zone is a valid target
+  if (dropZone.classList.contains('column')) {
+      dropZone.appendChild(draggedElement);
+  }
+}
