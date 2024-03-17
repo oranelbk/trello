e0f77a63d85b5fad52fe4e39850342a0686b1ecc
oranelbk <98135480+oranelbk@users.noreply.github.com>
2024-03-17 18:52:55 +0100

    Add files via upload

---
+++ trello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `trello`
+ */
+
+/* `trello`.`taches` */
+$taches = array(
+  array('id' => '16','titre' => 'titre de la tache ','description' => 'description de la tache 1 modifier','statut' => 'inProgress'),
+  array('id' => '17','titre' => 'qsdf','description' => 'vhj','statut' => 'done'),
+  array('id' => '18','titre' => 'la ça fonctionne','description' => 'ghjk','statut' => 'done')
+);
